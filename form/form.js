c0c5fc67751005bd62230020461bb0dcccf23661
jagadisholi <olijagadish1@gamil.com>
2024-06-09 23:14:51 +0545

    form backend

--- form/form.js
+++ form/form.js
@@ -2,4 +2,64 @@ function copyTemporaryToPermanent() {
     var temporaryAddress = document.getElementById('temporaryAddress').value;
     document.getElementById('permanentAddress').value = temporaryAddress;
 }
+function updateProvinces() {
+    const country = document.getElementById('country').value;
+    const provinceContainer = document.getElementById('province-container');
+    if (country === 'Nepal') {
+        provinceContainer.style.display = 'block';
+    } else {
+        provinceContainer.style.display = 'none';
+        resetFields();
+    }
+}
+
+function updateDistricts() {
+    const province = document.getElementById('province').value;
+    const districtContainer = document.getElementById('district-container');
+    const districtSelect = document.getElementById('district');
+    districtSelect.innerHTML = '<option value="" selected>Select District</option>'; // Reset districts
+
+    // Add your districts here based on the province
+    const districts = {
+        '1': ['District 1', 'District 2', 'District 3'],
+        '2': ['District 4', 'District 5', 'District 6'],
+        '3': ['District 7', 'District 8', 'District 9'],
+        '4': ['District 10', 'District 11', 'District 12'],
+        '5': ['District 13', 'District 14', 'District 15'],
+        '6': ['District 16', 'District 17', 'District 18'],
+        '7': ['District 19', 'District 20', 'District 21']
+    };
+
+    if (districts[province]) {
+        districts[province].forEach(district => {
+            const option = document.createElement('option');
+            option.value = district;
+            option.text = district;
+            districtSelect.add(option);
+        });
+        districtContainer.style.display = 'block';
+    } else {
+        districtContainer.style.display = 'none';
+        resetFields();
+    }
+}
+
+function showLocalityFields() {
+    document.getElementById('locality-container').style.display = 'block';
+    document.getElementById('village-container').style.display = 'block';
+    document.getElementById('ward-container').style.display = 'block';
+}
+
+function copyTemporaryToPermanent() {
+    const temporaryAddress = document.getElementById('temporaryAddress').value;
+    document.getElementById('permanentAddressCopy').value = temporaryAddress;
+}
+
+function resetFields() {
+    document.getElementById('district-container').style.display = 'none';
+    document.getElementById('locality-container').style.display = 'none';
+    document.getElementById('village-container').style.display = 'none';
+    document.getElementById('ward-container').style.display = 'none';
+}
+
 
---
+++ form/process_form.php
@@ -0,0 +1,32 @@
+<?php
+// This file will handle the form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $_POST['first_name'];
+    $middleName = $_POST['middle_name'];
+    $lastName = $_POST['last_name'];
+    $nationality = $_POST['nationality'];
+    $mobileNumber = $_POST['mobile_number'];
+    $dob = $_POST['dob'];
+    $email = $_POST['email'];
+    $course = $_POST['course'];
+    $fName = $_POST['f_name'];
+    $fOccupation = $_POST['f_occupation'];
+    $fMobileNo = $_POST['f_mobile_no'];
+    $mName = $_POST['m_name'];
+    $mOccupation = $_POST['m_occupation'];
+    $mMobileNo = $_POST['m_mobile_no'];
+    $gender = $_POST['gender'];
+    $country = $_POST['country'];
+    $province = isset($_POST['province']) ? $_POST['province'] : '';
+    $district = isset($_POST['district']) ? $_POST['district'] : '';
+    $locality = isset($_POST['locality']) ? $_POST['locality'] : '';
+    $village = isset($_POST['village']) ? $_POST['village'] : '';
+    $ward = isset($_POST['ward']) ? $_POST['ward'] : '';
+    $temporaryAddress = $_POST['temporaryAddress'];
+    $permanentAddressCopy = $_POST['permanentAddressCopy'];
+
+    // Add your form processing logic here (e.g., save data to database, send email, etc.)
+
+    echo "Form submitted successfully!";
+}
+?>
